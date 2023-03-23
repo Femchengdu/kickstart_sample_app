8ee0dad31cfbcdd902d308c108f7c4665cbde89f
Femi Ajayi <femiajayi705@gmail.com>
2023-03-23 13:16:30 +0100

    refac: Updated contract code

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -34,25 +34,28 @@ contract Campaign {
         _;
     }
 
-    function Campaign(uint minimum, address creator) public {
+    constructor(uint minimum, address creator) public {
         manager = creator;
         minimumContribution = minimum;
     }
 
     function contribute() public payable {
         require(msg.value > minimumContribution);
-
         approvers[msg.sender] = true;
         approversCount++;
     }
 
-    function createRequest(string description, uint value, address recipient) public restricted {
+    function createRequest(
+        string description,
+        uint value,
+        address recipient
+    ) public restricted {
         Request memory newRequest = Request({
-           description: description,
-           value: value,
-           recipient: recipient,
-           complete: false,
-           approvalCount: 0
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
         });
 
         requests.push(newRequest);
@@ -60,7 +63,6 @@ contract Campaign {
 
     function approveRequest(uint index) public {
         Request storage request = requests[index];
-
         require(approvers[msg.sender]);
         require(!request.approvals[msg.sender]);
 
@@ -68,12 +70,10 @@ contract Campaign {
         request.approvalCount++;
     }
 
-    function finalizeRequest(uint index) public restricted {
+    function finilizeRequest(uint index) public restricted {
         Request storage request = requests[index];
-
         require(request.approvalCount > (approversCount / 2));
         require(!request.complete);
-
         request.recipient.transfer(request.value);
         request.complete = true;
     }
