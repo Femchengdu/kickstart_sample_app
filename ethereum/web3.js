275e1447d190655116bf2ed13b901c6680f9618c
Femi Ajayi <femiajayi705@gmail.com>
2023-04-02 12:15:59 +0200

    refac: Added an if check to unsure the window is available

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,15 @@
 import Web3 from "web3";
 
-window.ethereum.request({ method: "eth_requestAccounts" });
-
-const web3 = new Web3(window.ethereum);
+let web3;
+try {
+  if (window) {
+    window.ethereum.request({ method: "eth_requestAccounts" });
+    web3 = new Web3(window.ethereum);
+  } else {
+    web3 = {};
+  }
+} catch (error) {
+  console.log("There is a problem with the window object", error.message);
+}
 
 export default web3;
