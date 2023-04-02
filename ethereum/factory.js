49684cb9a80947ed048550aebc4681de55a411de
Femi Ajayi <femiajayi705@gmail.com>
2023-04-02 12:16:56 +0200

    refac: Added an if check to ensure that web3 is availble in the factory

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,11 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const parsedCFInterface = JSON.parse(CampaignFactory.interface);
 const contractAddress = "0xf9612926c3a8e2274bE4D96A95D51ED8dC5e5427";
-
-const instance = new web3.eth.Contract(parsedCFInterface, contractAddress);
+let instance;
+if (web3 && web3.eth) {
+  instance = new web3.eth.Contract(parsedCFInterface, contractAddress);
+} else {
+  console.log("web3 not active yet", web3);
+}
 
 export default instance;
