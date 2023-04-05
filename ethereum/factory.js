d5fb4405b51cf06aa266fc4943ffb3d5a19bbba0
Femi Ajayi <femiajayi705@gmail.com>
2023-04-05 07:52:21 +0200

    refac: Factory code can run on the browser or server

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -2,7 +2,9 @@ import web3 from "./web3";
 import CampaignFactory from "./build/CampaignFactory.json";
 
 const parsedCFInterface = JSON.parse(CampaignFactory.interface);
-const contractAddress = process.env.CONTRACT_ADDRESS;
+const contractAddress =
+  process.env.CONTRACT_ADDRESS || process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
+
 let instance;
 if (web3 && web3.eth) {
   instance = new web3.eth.Contract(parsedCFInterface, contractAddress);
