656df1b14987c1803a6a9cd340c8c70964b313d3
Femi Ajayi <femiajayi705@gmail.com>
2023-04-05 06:19:29 +0200

    feat: Added header navigation

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
+import { Link } from "../routes";
 
 const Header = () => {
   return (
     <Menu style={{ marginTop: "10px" }}>
-      <Menu.Item>SomeCoine</Menu.Item>
+      <Link route="/">
+        <a className="item">SomeCoin</a>
+      </Link>
       <Menu.Menu position="right">
-        <Menu.Item>SomeCoine</Menu.Item>
-        <Menu.Item>SomeCoine</Menu.Item>
+        <Link route="/">
+          <a className="item">Campaigns</a>
+        </Link>
+        <Link route="/campaigns/new">
+          <a className="item">+</a>
+        </Link>
       </Menu.Menu>
     </Menu>
   );
